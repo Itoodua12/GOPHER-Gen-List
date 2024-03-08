32002de0d3b296452ca6ad77f4ae2ea5e5e15d11
Itoodua12 <itoodua@gmail.com>
2024-03-08 17:06:06 +0400

    refactore/project structure

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Itoodua12/GOPHER-Gen-List/internal/queue"
+	"github.com/Itoodua12/GOPHER-Gen-List/internal/genericstack"
+	"reflect"
+)
+
+func main() {
+
+	stack := genericstack.Stack{}
+
+	stack.Push(12)
+	stack.Push("Any type u want")
+	stack.Push(true)
+	stack.Push(3.14159)
+	stack.Push(func() {
+		fmt.Println("This is Function !!!!")
+	})
+
+	for !stack.IsEmpty() {
+		el, _ := stack.Pop()
+		if reflect.TypeOf(el).Kind() == reflect.Func {
+			fn, _ := el.(func())
+			fn()
+		} else {
+			fmt.Printf("Value is -> %v\n", el)
+		}
+	}
+
+	fmt.Println(" ##### QUEUE ##### ")
+
+	queue := queue.Queue{}
+
+	queue.Enqueue(13)
+	queue.Enqueue("GOpher")
+	queue.Enqueue(false)
+
+	for !queue.IsEmpty() {
+		el, _ := queue.Dequeue()
+		fmt.Printf("Value is -> %v\n", el)
+	}
+
+}
